98b379a51cc091ac4ca4aa43d97c31354609e4a6
nikeshskillup <nikesh_kr@skillup.online>
2023-10-17 04:17:16 -0400

    implemented django application

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_concert.settings")
@@ -14,9 +13,8 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        ) from exc  
     execute_from_command_line(sys.argv)
-
 
 if __name__ == "__main__":
     main()
